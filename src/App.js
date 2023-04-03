3bae36312da451486a3b2cf1e7869aeca7d6f256
Zbigniew Forys <zbigniew.forys@wp.pl>
2023-04-04 00:47:15 +0200

    Nev version GH

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import Form from "./Form";
+import TasksList from "./TasksList";
+import Buttons from "./Buttons";
+import Section from "./Section";
+import Container from "./Container";
+import "./index.css";
+
+const tasks = [
+  { id: 1, content: "Zadanie 1", done: true },
+  { id: 2, content: "Zadanie 2", done: false },
+];
+const hideTasks = false;
+
+
+
+function App() 
+return  (
+    <Container>
+      <h1 className="header">Lista zadań</h1>
+      <Section title="Dodaj nowe zadanie" 
+      buttons={""}  
+      body={<Form />}
+      />
+ 
+
+ <Section title="Lista zadań" 
+ buttons={<Buttons tasks={tasks} hideTasks={hideTasks} />}
+    body={<TasksList tasks={tasks} hideTasks={hideTasks}/>}
+    />
+     </Container>
+
   );
-}
 
-export default App;
+  export default App;
