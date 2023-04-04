a35b1fa2c05414746157f27763e3a7154be164ab
Zbigniew Forys <zbigniew.forys@wp.pl>
2023-04-05 01:12:20 +0200

    {}

--- src/Container/index.js
+++ src/Container/index.js
@@ -1,10 +1,9 @@
 
 import "./style.css";
-const Container=(props)=>{
- (
+const Container=(props)=> (
 <main className="container">
     {props.children}
 </main>
-); }
+); 
 export default Container;
    
